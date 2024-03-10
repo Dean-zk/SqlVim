4ee403718d355236b1b69e4b33de8307e233660a
Dean <119504721+Dean-zk@users.noreply.github.com>
2024-03-10 16:13:01 +0100

    Add files via upload

---
+++ nvim/lua/vimrc.lua
@@ -0,0 +1,9 @@
+--Add all vim related settings here. NOT IDE RELATED.
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+
+vim.g.mapleader = " "
