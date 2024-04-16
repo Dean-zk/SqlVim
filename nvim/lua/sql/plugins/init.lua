fcbbdaee41d81dafd7fe094f69683952471da4b3
Dean <119504721+SqLait@users.noreply.github.com>
2024-04-16 22:47:28 +0200

    SqLVim v2.0.0
    check it out cuhh

---
+++ nvim/lua/sql/plugins/init.lua
@@ -0,0 +1,5 @@
+-- declare one liners that don't need configuration
+return {
+  "nvim-lua/plenary.nvim", -- popular plugin dependency
+  "christoomey/vim-tmux-navigator", -- Tmux & NeoVim navigator
+}
