d4854bba4669278e23f2bdcca6686a1e4eadf62c
Dean <119504721+SqLait@users.noreply.github.com>
2024-04-30 11:25:30 +0200

    Delete nvim directory in preperation for the next push
    Nothing to worry about.

--- nvim/lua/sql/plugins/init.lua
+++
@@ -1,5 +0,0 @@
--- declare one liners that don't need configuration
-return {
-  "nvim-lua/plenary.nvim", -- popular plugin dependency
-  "christoomey/vim-tmux-navigator", -- Tmux & NeoVim navigator
-}
