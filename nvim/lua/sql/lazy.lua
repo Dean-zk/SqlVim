8e58f8bde2b78a286114b5d618fbf579d9ccae1f
Dean <119504721+SqLait@users.noreply.github.com>
2024-04-30 11:28:40 +0200

    SqLVim v2.4.0, better dir update.
    In this update I basically grabbed all the files and put them in either a shared dir or single dir. Now it will be easier to find the files you are looking for. The structure is not perfect now, and gitsigns.nvim is also missing. Don't worry this wil get fixed later today. Happy vimming!

---
+++ nvim/lua/sql/lazy.lua
@@ -0,0 +1,27 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", --latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  { import = "sql.plugins.lsp" }, { import = "sql.plugins.modules" },
+  { import = "sql.plugins.ui" }, { import = "sql.plugins.terminal"}, { import = "sql.plugins.dependencies" },
+  { import = "sql.plugins.comments"}, { import = "sql.plugins.syntax" }, { import = "sql.plugins.misc" },
+  { import = "sql.plugins.completion"} },
+  {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
