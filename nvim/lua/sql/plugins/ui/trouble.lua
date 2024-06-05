496e294c0ba5c9cf8d3c1e967112e0276be9bd50
SqL <119504721+SqLait@users.noreply.github.com>
2024-06-05 15:44:22 +0200

    SqLVim v2.7.7 | Fixed troubles breaking changes

--- nvim/lua/sql/plugins/ui/trouble.lua
+++ nvim/lua/sql/plugins/ui/trouble.lua
@@ -1,14 +1,13 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = "Trouble",
   opts = {
     height = 7
   },
   keys = {
-    { "<leader>xx", "<cmd>TroubleToggle<CR>", desc = "Open/close trouble list" },
-    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>TroubleToggle quickfix<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>TroubleToggle loclist<CR>", desc = "Open trouble location list" },
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Open/close trouble list" },
+    { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Open trouble quickfix list" },
+    { "<leader>xl", "<cmd>Trouble loclist toggle<cr>", desc = "Open trouble location list" },
   },
 }
